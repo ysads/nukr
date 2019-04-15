925fb44a6032567211f1a7978e8d70e230329cdd
Ygor Sad <ysads2010@gmail.com>
2019-04-15 13:08:57 -0300

    [deps] add compojure and ring to dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,10 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.9.0"]
+                 [com.stuartsierra/component "0.4.0"]
                  [buddy/buddy-hashers "1.3.0"]
-                 [com.stuartsierra/component "0.4.0"]]
+                 [ring/ring-jetty-adapter "1.6.3"]
+                 [metosin/compojure-api "1.1.11"]]
   :main ^:skip-aot nukr.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
