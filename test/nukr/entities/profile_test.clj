bc8f14516b59ad875620a739a84e4fd612fd4879
Ygor Sad <ysads2010@gmail.com>
2019-04-15 17:28:49 -0300

    [profile-test] subst. deftest macro with testing
    This aims to make tests more descritive, and, the debugging process,
    more pleasant and assertive

--- test/nukr/entities/profile_test.clj
+++ test/nukr/entities/profile_test.clj
@@ -2,6 +2,8 @@
   (:require [clojure.test :refer :all]
             [nukr.entities.profile :refer :all]))
 
+;; Auxiliary functions intended to help testing the
+;; profile namespace
 (defn with-uuid
   "Returns a copy of the given object with a UUID set"
   [data]
@@ -27,43 +29,43 @@
                         :connections
                         [(:uuid (with-uuid public-profile))]))
 
-(testing "profile-privacy"
-  (deftest private-profile-privacy
+(testing "profile/private?"
+  (testing "for private profiles"
     (is (private? private-profile)))
 
-  (deftest public-profile-privacy
+  (testing "for public profiles"
     (is (not (private? public-profile)))))
 
-(testing "password-hashing"
-  (deftest plain-password-dissoc
-    (let [dissoc-data (dissoc-plain-password public-profile)]
-      (is (false? (contains? dissoc-data :password)))))
+(testing "profile/dissoc-plain-password removes :password from profile"
+  (let [dissoc-data (dissoc-plain-password public-profile)]
+    (is (false? (contains? dissoc-data :password)))))
 
-  (deftest when-no-password-exists
+(testing "profile/with-hashed-password"
+  (testing "throws exception if profile doesn't have passowrds"
     (let [broken-data {:name "Elizabeth McQueen"}]
       (is (thrown? java.lang.NoSuchFieldError
                    (with-hashed-password broken-data)))))
-
-  (deftest when-password-exists
+  
+  (testing "hashes password and assoc it to :password-hash"
     (let [data-with-password (with-hashed-password public-profile)]
       (is (true? (contains? data-with-password :password-hash)))
       (is (false? (contains? data-with-password :password))))))
 
-(testing "connections-initializing"
-  (deftest init-empty-connections
+(testing "profile/with-connections"
+  (testing "init a ref to an empty set if there is no connection"
     (let [profile (with-connections public-profile)]
       (is (instance? clojure.lang.Ref (:connections profile)))
       (is (empty? @(:connections profile)))))
 
-  (deftest init-existing-collections
+  (testing "init a ref to a set with the available connections"
     (let [profile (with-connections connected-profile)]
       (is (instance? clojure.lang.Ref (:connections profile)))
       (is (false? (->> @(:connections profile)
                        (remove nil?)
                        (empty?)))))))
 
-(testing "privacy-settings"
-  (deftest when-private-key-is-explicit
+(testing "profile/with-privacy-settings"
+  (testing "keeps :private field of profile if it exists"
     (let [pub-profile (assoc public-profile :private false)]
       (is (-> (with-privacy-settings pub-profile)
               (:private)
@@ -72,26 +74,27 @@
               (:private)
               (true?))))))
 
-  (deftest when-privacy-missing
+  (testing "assoc false to :private field if it doesn't exist"
     (let [profile (with-privacy-settings public-profile)]
       (is (false? (:private profile))))))
 
-(deftest profiles-creation
-  (let [profile (create-profile public-profile)]
-    (is (instance? nukr.entities.profile.Profile profile))
-    (is (instance? clojure.lang.Ref (:connections profile)))
-    (is (false? (:private profile)))
-    (is (false? (contains? profile :password)))))
+(testing "profile/create-profile"
+  (testing "creates instance of Profile with connections and hashed password"
+    (let [profile (create-profile public-profile)]
+      (is (instance? nukr.entities.profile.Profile profile))
+      (is (instance? clojure.lang.Ref (:connections profile)))
+      (is (false? (:private profile)))
+      (is (false? (contains? profile :password))))))
 
-(testing "connected?"
-  (deftest when-connected-profiles
+(testing "profile/connected?"
+  (testing "when profiles are connected"
     (let [a (stub-profile public-profile)
           b (stub-profile another-profile)]
       (connect! a b)
       (is (connected? a b))
       (is (connected? b a))))
-    
-  (deftest when-not-connected-profiles
+
+  (testing "when profiles are not connected"
     (let [a (stub-profile public-profile)
           b (stub-profile another-profile)]
       (is (not (connected? a b)))
