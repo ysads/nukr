1c45c82d393aa0368463c2930a1cd3d0534bdd53
Ygor Sad <ysads2010@gmail.com>
2019-05-20 16:22:03 -0300

    [core] implements main

--- src/nukr/core.clj
+++ src/nukr/core.clj
@@ -1,7 +1,7 @@
 (ns nukr.core
+  (:require [nukr.system :as sys])
   (:gen-class))
 
-(defn -main
-  "I don't do a whole lot ... yet."
-  [& args]
-  (println "Hello, World!"))
+(defn -main [& args]
+  (sys/init-system!)
+  (sys/start!))
--- test/nukr/core_test.clj
+++
@@ -1,7 +0,0 @@
-(ns nukr.core-test
-  (:require [clojure.test :refer :all]
-            [nukr.core :refer :all]))
-
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 1 1))))
