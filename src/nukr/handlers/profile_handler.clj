af4c86124ba6e0a6d936d2c9177bad227b6827ee
Ygor Sad <ysads2010@gmail.com>
2019-05-20 15:53:06 -0300

    [profile-handler] overall refactor

--- src/nukr/handlers/profile_handler.clj
+++ src/nukr/handlers/profile_handler.clj
@@ -1,41 +1,58 @@
 (ns nukr.handlers.profile-handler
+  (:use [clojure.pprint])
   (:require [nukr.entities.profile :refer [create-profile connect! suggestions]]
             [nukr.storage.in-memory :as db]
-            [ring.util.http-response :refer [ok not-found created]])
-  (:import java.util.NoSuchElementException)
+            [ring.util.http-response :refer [ok bad-request not-found created method-not-allowed]])
+  (:import java.util.NoSuchElementException
+           java.lang.NoSuchFieldError)
   (:gen-class))
 
-(defn uuid->created
+(defn- uuid->created
   "Returns a `201 Created` HTTP response based on an UUID"
   [uuid]
-  (-> (str "/profile/" uuid)
+  (-> (str "/profiles/" uuid)
       (created {:uuid uuid})))
 
 (defn create-profile-handler
   "Creates a profile based on data received as argument,
   saving it to storage"
   [storage request]
-  (->> (:form-params request)
-       (create-profile)
-       (db/insert! storage)
-       (:uuid)
-       (uuid->created)))
+  (if (= :post (:request-method request))
+    (try
+      (->> (create-profile (:profile (:params request)))
+           (db/insert! storage)
+           (:uuid)
+           (uuid->created))
+      (catch NoSuchFieldError ex
+        (bad-request)))
+    (method-not-allowed)))
 
-(defn with-privacy-set
+(defn- with-privacy-set
   "Returns a profile with it's privacy set according the
   request setting"
   [private? profile]
   (assoc profile :private private?))
 
+
+(defn- deref-connections
+  "Returns a profile with its connections attribute dereferenced.
+  This allows the object to be safely JSON-encoded."
+  [profile]
+  (if (instance? clojure.lang.Ref (:connections profile))
+    (update-in profile [:connections] deref)
+    profile))
+
 (defn opt-profile-handler
   "Toggles a profile privacy state, that is, set a profile
   as public if it's private or vice-versa"
   [storage request]
-  (let [{:keys [uuid private]} (:route-params request)]
+  (let [{:keys [uuid private]} (:params request)]
     (try
        (->> (db/get-by-uuid! storage uuid)
             (with-privacy-set private)
             (db/update-by-uuid! storage)
+            (deref-connections)
+            (assoc {} :profile)
             (ok))
        (catch NoSuchElementException ex
          (not-found)))))
@@ -44,7 +61,7 @@
   "Tag two profiles as connected, if they are not yet
   connected. Otherwise, does nothing"
   [storage request]
-  (let [{:keys [uuid-a uuid-b]} (:route-params request)]
+  (let [{:keys [uuid-a uuid-b]} (:params request)]
     (try
       (let [profile-a (db/get-by-uuid! storage uuid-a)
             profile-b (db/get-by-uuid! storage uuid-b)]
@@ -59,20 +76,37 @@
 ;;; when this argument is not explicitly passed
 (def default-suggest-num 5)
 
-(defn suggestions->ok
+(defn- parse-suggestions-count
+  "Returns the suggestions count sent in the request or
+  the default count otherwise."
+  [request]
+  (let [num (:count (:params request))]
+    (cond
+      (string? num) (Integer/parseInt num)
+      (nil? num) default-suggest-num
+      :else num)))
+
+(defn- suggestions->ok
   "Returns a `200 ok` HTTP response containing a collection
-  of profile suggestions"
+  of profile suggestions. Note this profiles have their connections
+  dereferenced."
   [suggestions]
-  (ok {:suggestions suggestions}))
+  (->> suggestions
+       (map #(update-in % [:profile] deref-connections))
+       (doall)
+       (assoc {} :suggestions)
+       (ok)))
 
 (defn suggestions-handler
   "Returns a collection of connection suggestions for
   a given profile based upon its existing connections"
   [storage request]
-  (let [{:keys [uuid count]} (:route-params request)]
+  ; (println "\n\nREQUEST")
+  ; (pprint request)
+  (let [{:keys [uuid]} (:params request)]
     (try
       (let [profile (db/get-by-uuid! storage uuid)
-            num (or count default-suggest-num)]
+            num     (parse-suggestions-count request)]
         (->> (suggestions storage profile num)
              (suggestions->ok)))
       (catch NoSuchElementException ex
--- test/nukr/handlers/profile_handler_test.clj
+++ test/nukr/handlers/profile_handler_test.clj
@@ -15,15 +15,16 @@
 ;;; a more real use scenario.
 (def storage (.start (db/init-storage)))
 
-(def success-create-req {:form-params {:name "John Doe"
-                                       :email "john.doe@example.com"
-                                       :password "123456"
-                                       :gender "other"}})
+(def success-create-req {:request-method :post
+                         :params {:profile {:name "John Doe"
+                                            :email "john.doe@example.com"
+                                            :password "123456"
+                                            :gender "other"}}})
 
 (defn stub-request
   "Stub a request map with route-params key set"
   [params]
-  {:route-params params})
+  {:params params})
 
 (defn stub-profile-uuid
   "Stub a new profile using create-profile-handler"
@@ -49,12 +50,23 @@
     uuid-a))
 
 (defn suggestion?
-  "Returns if the given map is structurally a suggestion"
+  "Returns true if the given map contains :relevance
+  and :profile entries"
   [data]
   (and (contains? data :relevance)
        (contains? data :profile)))
 
 (testing "create-profile-handler"
+  (testing "when password is missing"
+    (let [bad-req  (update-in success-create-req [:params :profile] dissoc :password)
+          response (create-profile-handler storage bad-req)]
+      (is (= 400 (:status response)))))
+
+  (testing "when request method is not POST"
+    (let [bad-req  (assoc success-create-req :request-method :get)
+          response (create-profile-handler storage bad-req)]
+      (is (= 405 (:status response)))))
+
   (testing "when request data is valid"
     (let [response (create-profile-handler storage success-create-req)]
       (is (= 201 (:status response)))
@@ -62,23 +74,27 @@
 
 (testing "opt-profile-handler"
   (testing "returns 404 if profile not found"
-    (let [request (stub-request {:uuid "1234" :private true})
+    (let [request  (stub-request {:uuid "1234" :private true})
           response (opt-profile-handler storage request)]
       (is (= 404 (:status response)))))
 
   (testing "when transition is public->private"
-    (let [uuid (stub-profile-uuid success-create-req)
-          request (stub-request {:uuid uuid :private true})]
-      (is (= 200 (:status (opt-profile-handler storage request))))
-      (is (->> (db/get-by-uuid! storage uuid)
-               (private?)))))
+    (let [uuid     (stub-profile-uuid success-create-req)
+          request  (stub-request {:uuid uuid :private true})
+          response (opt-profile-handler storage request)
+          profile  (:profile (:body response))]
+      (is (= 200 (:status response)))
+      (is (private? profile))
+      (is (not (instance? clojure.lang.Ref (:connections profile))))))
 
   (testing "when transition is private->public"
-    (let [uuid (stub-profile-uuid success-create-req)
-          request (stub-request {:uuid uuid :private false})]
-      (is (= 200 (:status (opt-profile-handler storage request))))
-      (is (not (->> (db/get-by-uuid! storage uuid)
-                    (private?)))))))
+    (let [uuid     (stub-profile-uuid success-create-req)
+          request  (stub-request {:uuid uuid :private false})
+          response (opt-profile-handler storage request)
+          profile  (:profile (:body response))]
+      (is (= 200 (:status response)))
+      (is (not (private? profile)))
+      (is (not (instance? clojure.lang.Ref (:connections profile)))))))
 
 (testing "connect-profiles-handler"
   (testing "returns 404 if either of profiles not found"
@@ -86,8 +102,8 @@
       (is (= 404 (:status (connect-profiles-handler storage request))))))
 
   (testing "when profiles are not connected"
-    (let [uuid-a (stub-profile-uuid success-create-req)
-          uuid-b (stub-profile-uuid success-create-req)
+    (let [uuid-a  (stub-profile-uuid success-create-req)
+          uuid-b  (stub-profile-uuid success-create-req)
           request (stub-request {:uuid-a uuid-a :uuid-b uuid-b})]
       (is (not (connected? (db/get-by-uuid! storage uuid-a)
                            (db/get-by-uuid! storage uuid-b))))
@@ -104,18 +120,18 @@
       (is (= 404 (:status (suggestions-handler storage request))))))
 
   (testing "returns an array with at most n items when requested"
-    (let [uuid (stub-connected-profile-uuid! storage)
-          request (stub-request {:uuid uuid :count 1})
-          response (suggestions-handler storage request)
+    (let [uuid         (stub-connected-profile-uuid! storage)
+          request      (stub-request {:uuid uuid :count 1})
+          response     (suggestions-handler storage request)
           suggest-list (:suggestions (:body response))]
       (is (= 200 (:status response)))
       (is (= 1 (count suggest-list)))
       (is (every? suggestion? suggest-list))))
 
   (testing "returns an array of suggestions of available suggestions"
-    (let [uuid (stub-connected-profile-uuid! storage)
-          request (stub-request {:uuid uuid})
-          response (suggestions-handler storage request)
+    (let [uuid         (stub-connected-profile-uuid! storage)
+          request      (stub-request {:uuid uuid})
+          response     (suggestions-handler storage request)
           suggest-list (:suggestions (:body response))]
       (is (= 200 (:status response)))
       (is (= 2 (count suggest-list)))
