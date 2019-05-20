05b35f2c99e2cc3f63e6e1bde59644678f08d435
Ygor Sad <ysads2010@gmail.com>
2019-05-20 00:23:03 -0300

    [deps] adds new dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,16 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [com.stuartsierra/component "0.4.0"]
+  :dependencies [[aleph "0.4.6"]
+                 [bidi "2.1.6"]
                  [buddy/buddy-hashers "1.3.0"]
+                 [cheshire "5.8.1"]
+                 [com.stuartsierra/component "0.4.0"]
+                 [metosin/ring-http-response "0.9.1"]
+                 [org.clojure/clojure "1.9.0"]
                  [ring/ring-jetty-adapter "1.6.3"]
-                 [metosin/compojure-api "1.1.11"]]
+                 [ring/ring-json "0.4.0"]]
+  :user {:plugins [[venantius/ultra "0.6.0"]]}
   :main ^:skip-aot nukr.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
