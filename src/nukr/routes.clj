c8c2b462f79afbcaebc12538fa0a901256dd8ce0
Ygor Sad <ysads2010@gmail.com>
2019-05-20 00:24:01 -0300

    [routes] creates the routes matcher

---
+++ src/nukr/routes.clj
@@ -0,0 +1,19 @@
+(ns nukr.routes
+  (:use [clojure.pprint])
+  (:require [bidi.ring :refer [make-handler]]
+            [nukr.handlers.profile-handler :refer :all])
+  (:gen-class))
+
+(defn routes
+  "Returns a data structure mapping each route to its
+  associated handler function"
+  [storage]
+  ["/profiles" {""                                (partial create-profile-handler storage)
+                ["/" :uuid "/opt/" :private]      (partial opt-profile-handler storage)
+                ["/" :uuid "/suggestions"]        (partial suggestions-handler storage)
+                ["/connect/" :uuid-a "/" :uuid-b] (partial connect-profiles-handler storage)}])
+
+(defn routes-handler
+  "Defines the matching handler for each route of the app"
+  [storage]
+  (make-handler (routes storage)))
