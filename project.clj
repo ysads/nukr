4ad9c1dcdc059100d39549d0d29278feec7a301e
Ygor Sad <ysads2010@gmail.com>
2019-05-22 04:18:37 -0300

    [project] remove unused deps

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject nukr "0.1.0-SNAPSHOT"
   :description "A prototype of social network built using functional programming techniques and features."
-  :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[aleph "0.4.6"]
@@ -10,7 +9,6 @@
                  [com.stuartsierra/component "0.4.0"]
                  [metosin/ring-http-response "0.9.1"]
                  [org.clojure/clojure "1.9.0"]
-                 [ring/ring-jetty-adapter "1.6.3"]
                  [ring/ring-json "0.4.0"]]
   :user {:plugins [[venantius/ultra "0.6.0"]]}
   :main ^:skip-aot nukr.core
